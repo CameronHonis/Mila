32bd7fe3f982233de1f6bafee74553a7297e94e1
Cameron <cameron-04@hotmail.com>
2024-04-10 10:10:12 -0500

    add off-ramp test search in main.go

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/CameronHonis/chess"
+	"log"
 )
 
 func main() {
 	fmt.Println("Mila v0.0.0 - a lightweight chess AI written in go by Cameron Honis")
-	NewUci(NewTranspTable()).Start()
+	tt := NewTranspTable()
+	go NewUci(tt).Start()
+	StartSearchTest(tt)
+}
+
+func StartSearchTest(tt *TranspTable) {
+	pos, posErr := chess.BoardFromFEN(" 1r1q3r/pBP2pbp/1p2p1pn/4P2k/4QP2/B4N1P/P5P1/R4RK1 w - - 1 19")
+	if posErr != nil {
+		log.Fatalf("pos error %s", posErr)
+	}
+	NewSearch(pos, &SearchConstraints{maxDepth: 4}, tt).Start()
 }
