bdd7674f2df2ff37f05933b7295d8ac6f4635a52
Cameron <cameron-04@hotmail.com>
2024-04-10 14:33:34 -0500

    add bitboard with String repr

---
+++ bitboard.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Bitboard uint64
+
+func (b Bitboard) String() string {
+	var rtnBuilder = strings.Builder{}
+	for rank := 8; rank > 0; rank-- {
+		rtnBuilder.WriteString(fmt.Sprintf("%d|", rank))
+		var mask Bitboard
+		for file := 1; file < 9; file++ {
+			idx := 8*(rank-1) + (file - 1)
+			mask = 1 << idx
+			if mask&b > 0 {
+				rtnBuilder.WriteString("██")
+			} else {
+				var isDark bool
+				if rank%2 == 0 {
+					isDark = file%2 == 0
+				} else {
+					isDark = file%2 == 1
+				}
+				if isDark {
+					rtnBuilder.WriteString("░░")
+				} else {
+					rtnBuilder.WriteString("  ")
+				}
+			}
+		}
+		rtnBuilder.WriteByte('\n')
+	}
+	rtnBuilder.WriteString("――――――――――――――――――\n |")
+	for file := 1; file < 9; file++ {
+		rtnBuilder.WriteByte(byte('0' + file))
+		rtnBuilder.WriteByte(' ')
+	}
+	return rtnBuilder.String()
+}
---
+++ bitboard_test.go
@@ -0,0 +1,29 @@
+package main_test
+
+import (
+	"fmt"
+	main "github.com/CameronHonis/Mila"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Bitboard", func() {
+	Describe("::String", func() {
+		It("Generates a multi-line string of a board that represents the bitboard", func() {
+			//var bitb main.Bitboard = 0b01001000_10000100_00010000_00100000_00100000_00010000_00000100_00000000
+			var bitb main.Bitboard = 0b00000000_00100000_00001000_00000100_00000100_00001000_00100001_00010010
+			expStr := "" +
+				"8|  ░░  ░░  ░░  ░░\n" +
+				"7|░░  ░░  ░░██░░  \n" +
+				"6|  ░░  ██  ░░  ░░\n" +
+				"5|░░  ██  ░░  ░░  \n" +
+				"4|  ░░██░░  ░░  ░░\n" +
+				"3|░░  ░░██░░  ░░  \n" +
+				"2|██░░  ░░  ██  ░░\n" +
+				"1|░░██░░  ██  ░░  \n" +
+				"――――――――――――――――――\n" +
+				" |1 2 3 4 5 6 7 8 "
+			Expect(bitb.String()).To(Equal(expStr), fmt.Sprintf("%s\nis not\n%s", bitb.String(), expStr))
+		})
+	})
+})
